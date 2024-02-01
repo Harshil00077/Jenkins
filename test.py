360fa29aeb3656c84d0051e9a7a181b3d43935bb
Harshil00077 <155606208+Harshil00077@users.noreply.github.com>
2024-02-01 10:49:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ from Prog1 import sum
 
 class TestSum(unittest.TestCase):
   def test_list_int(self):
-    data = [10,9]
+    a = 10
+    b = 20
+    data = [a,b]
     result = sum(data)
-    self.assertEqual(resutl,55)
+    self.assertEqual(resutl,a+b)
 
 if __name__ = '__main__':
   unittest.main()
